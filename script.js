ffb1380549022ccfd45c8b48d661b7c987fda5ca
rohanmittal <rohanmittal@gmail.com>
2021-01-09 23:19:05 +0530

    Basic Console print ready

---
+++ script.js
@@ -0,0 +1,66 @@
+class book {
+
+    contructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title : 'Mind reading',
+                author: 'swetabh',
+                isbn: '3454'
+            },
+            {
+                title:'Hello java ',
+                author: 'michale',
+                isbn : '3444'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBooksToList(book));
+    }
+
+
+    static addBooksToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>   
+        <td><a href="#" class="btn btn-danger btn-sm delete">
+        X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+
+}
+
+
+document.addEventListener('DOMContentLoaded',UI.displayBooks);
+
+
+document.querySelector('#book-form').addEventListener('submit',(e) =>{
+
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    const Books = new book(title,author,isbn);
+
+});
+
+
